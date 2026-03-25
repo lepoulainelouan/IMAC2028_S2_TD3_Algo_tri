ca12241c9eb5e87cae0472d6092d411b223cdabf
Elouan <elouan.lepoulain@edu.esiee.fr>
2026-03-25 08:50:20 +0100

    correction exo1

--- src/exo1.cpp
+++ src/exo1.cpp
@@ -1,11 +1,20 @@
 #include <vector>
+#include <iostream>
 #include <algorithm>
 
 
 bool is_sorted(std::vector<int> const& vec) { return std::is_sorted(vec.begin(), vec.end()); }
 
 //tri par selection
-std::vector<int> array {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int main (){
+    std::vector<int> array {1, 2, 3, 4, 7, 6, 5, 8, 9};
+        if (is_sorted(array)) {
+            std::cout << "Le tableau est trié" << std::endl;
+        } else {
+            std::cout << "Le tableau n'est pas trié" << std::endl;
+        }   
+}
+
 void selection_sort(std::vector<int>& vec) {
     for (size_t i = 0; i < vec.size(); ++i) {
         size_t min = i;
@@ -17,3 +26,4 @@ void selection_sort(std::vector<int>& vec) {
         std::swap(vec[i], vec[min]);
     }
 }
+
