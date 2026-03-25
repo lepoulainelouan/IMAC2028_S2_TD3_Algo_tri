24fa60dee6cda3e0817da73542a44a70182489a9
Elouan <elouan.lepoulain@edu.esiee.fr>
2026-03-25 08:46:28 +0100

    exo1_tripar selection

--- src/exo1.cpp
+++ src/exo1.cpp
@@ -1,70 +1,19 @@
-#include <cassert>
-#include <charconv>
-#include <iostream>
-#include <optional>
-#include <string>
 #include <vector>
-#include <sstream>
-#include <iterator>
-#include <stack>
-#include <cmath>
-#include <system_error>
+#include <algorithm>
 
-//exercice 3 : Conversion en NPI, convertire en notation infixe 
-//Algorithme de Shunting Yard, creation de la fonction infix_to_npi_tokens
 
-enum class Operator { ADD, SUB, MUL, DIV, POW, OPEN_PAREN, CLOSE_PAREN };
-enum class TokenType { OPERATOR, OPERAND };
+bool is_sorted(std::vector<int> const& vec) { return std::is_sorted(vec.begin(), vec.end()); }
 
-void infix_to_npi_tokens(std::string const& expression) {
-    std::stack<Operator> operators_stack {};
-    std::vector<Token> output {};
-
-    std::vector<Token> const tokens { tokenize(split_string(expression)) };
-
-    for(Token const& token : tokens)
-    {
-        if (token.type == TokenType::OPERAND)
-        {
-            output.push_back(token);
-        }
-        else
-        {
-            Operator const& op { token.op };
-
-            if (op == Operator::OPEN_PAREN)
-            {
-                operators_stack.push(op);
-            }
-            else if (op == Operator::CLOSE_PAREN)
-            {
-                while (!operators_stack.empty() && operators_stack.top() != Operator::OPEN_PAREN) {
-                    output.push_back(make_token(operators_stack.top()));
-                    operators_stack.pop();
-                }
-                if (!operators_stack.empty() && operators_stack.top() == Operator::OPEN_PAREN) {
-                    operators_stack.pop();
-                } else {
-                    throw std::runtime_error("soucis avec les parenthèses la mon reuf");
-                }
-            }
-            else {
-                while (!operators_stack.empty() && operator_precedence(operators_stack.top()) >= operator_precedence(op)) {
-                    output.push_back(make_token(operators_stack.top()));
-                    operators_stack.pop();
-                }
-                operators_stack.push(op);
+//tri par selection
+std::vector<int> array {1, 2, 3, 4, 5, 6, 7, 8, 9};
+void selection_sort(std::vector<int>& vec) {
+    for (size_t i = 0; i < vec.size(); ++i) {
+        size_t min = i;
+        for (size_t j = i + 1; j < vec.size(); ++j) {
+            if (vec[j] < vec[min]) {
+                min=j;
             }
         }
+        std::swap(vec[i], vec[min]);
     }
-
-    while (!operators_stack.empty()) {
-        if (operators_stack.top() == Operator::OPEN_PAREN || operators_stack.top() == Operator::CLOSE_PAREN) {
-            throw std::runtime_error("Erreur avec les parenthèses la mon reuf");
-        }
-        output.push_back(make_token(operators_stack.top()));
-        operators_stack.pop();
-    }
-
-    return output;
 }
