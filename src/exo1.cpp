644645aacfd9d702ade2b9936e0c46cf5389c7bc
Elouan <elouan.lepoulain@edu.esiee.fr>
2026-03-25 09:08:58 +0100

    add gitignore

--- src/exo1.cpp
+++ src/exo1.cpp
@@ -6,20 +6,11 @@
 bool is_sorted(std::vector<int> const& vec) { return std::is_sorted(vec.begin(), vec.end()); }
 
 //tri par selection
-int main (){
-    std::vector<int> array {1, 2, 3, 4, 7, 6, 5, 8, 9};
-        if (is_sorted(array)) {
-            std::cout << "Le tableau est trié" << std::endl;
-        } else {
-            std::cout << "Le tableau n'est pas trié" << std::endl;
-        }   
-}
-
 void selection_sort(std::vector<int>& vec) {
     for (size_t i = 0; i < vec.size(); ++i) {
         size_t min = i;
-        for (size_t j = i + 1; j < vec.size(); ++j) {
-            if (vec[j] < vec[min]) {
+        for (size_t j = i + 1; j<vec.size(); ++j) {
+            if (vec[j]<vec[min]) {
                 min=j;
             }
         }
@@ -27,3 +18,12 @@ void selection_sort(std::vector<int>& vec) {
     }
 }
 
+int main (){
+    std::vector<int> array {1, 67, 42, 4, 11, 6, 5, 8, 9};
+    selection_sort(array);
+        if (is_sorted(array)) {
+            std::cout << "Le tableau est tout propre" << std::endl;
+        } else {
+            std::cout << "Le tableau n'est pas propre du tout >:(" << std::endl;
+        }   
+}
--- src/exo2.cpp
+++ src/exo2.cpp
@@ -1,3 +1,14 @@
 #include <vector>
+#include <iostream>
 #include <algorithm>
 bool is_sorted(std::vector<int> const& vec) { return std::is_sorted(vec.begin(), vec.end()); }
+
+int main (){
+    std::vector<int> array {1, 2, 3, 4, 7, 6, 5, 8, 9};
+        if (is_sorted(array)) {
+            std::cout << "Le tableau est trié" << std::endl;
+        } else {
+            std::cout << "Le tableau n'est pas trié" << std::endl;
+        }   
+}
+
