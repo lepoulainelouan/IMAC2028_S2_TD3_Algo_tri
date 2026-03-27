ad200f9ec8ec69bfdb0f646c8c06b5785b1a4851
Elouan <elouan.lepoulain@edu.esiee.fr>
2026-03-27 18:52:28 +0100

    Stop tracking build folder

---
+++ src/exo3.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+
+std::vector<int> generate_random_vector(size_t const size, int const max = 100) {
+    std::vector<int> vec(size);
+    std::generate(vec.begin(), vec.end(), [&max]() { return std::rand() % max;} );
+    return vec;
+}
+
+#include "ScopedTimer.hpp"
+
+
+{
+
+ScopedTimer timer("nom du chronomètre");
+// code à chronométrer
+}
+
+std::vector<int> array {1, 2, 3, 4, 5, 6, 7, 8, 9};
+std::sort(array.begin(), array.end());
---
+++ src/exo4.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+//algo de dichotomie
+bool dichotomie(std::vector<int> const& vec, int const value) {
+    size_t left = 0;
+    size_t right = vec.size() - 1;
+    while (left <= right) {
+        size_t mid = left + (right - left) / 2;
+        if (vec[mid] == value) {
+            return true;
+        } else if (vec[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+int main (){
+    std::vector<int> array {1, 67, 42, 4, 11, 6, 5, 8, 9};
+    bool found = dichotomie(array, 42);
+    if (found) {
+        std::cout << "Valeur trouvée" << std::endl;
+    } else {
+        std::cout << "Valeur non trouvée" << std::endl;
+    }
+}
+
